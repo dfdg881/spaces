b11cc03ed6d573594d6dea386fa648c30acf3935
dfdg881 <114667012+dfdg881@users.noreply.github.com>
2026-01-02 11:04:49 +0800

    更新 docker-entrypoint.sh

--- QingLong/docker-entrypoint.sh
+++ QingLong/docker-entrypoint.sh
@@ -9,9 +9,6 @@ echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 
 echo -e "======================1. 检测配置文件========================\n"
 import_config "$@"
-make_dir /etc/nginx/conf.d
-make_dir /run/nginx
-init_nginx
 fix_config
 
 pm2 l &>/dev/null
@@ -19,23 +16,18 @@ pm2 l &>/dev/null
 echo -e "======================2. 安装依赖========================\n"
 patch_version
 
-
-echo -e "======================3. 启动nginx========================\n"
-nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
-echo -e "nginx启动成功...\n"
-
-echo -e "======================4. 启动pm2服务========================\n"
+echo -e "======================3. 启动pm2服务========================\n"
 reload_update
 reload_pm2
 
 if [[ $AutoStartBot == true ]]; then
-  echo -e "======================5. 启动bot========================\n"
+  echo -e "======================4. 启动bot========================\n"
   nohup ql bot >$dir_log/bot.log 2>&1 &
   echo -e "bot后台启动中...\n"
 fi
 
 if [[ $EnableExtraShell == true ]]; then
-  echo -e "====================6. 执行自定义脚本========================\n"
+  echo -e "====================5. 执行自定义脚本========================\n"
   nohup ql extra >$dir_log/extra.log 2>&1 &
   echo -e "自定义脚本后台执行中...\n"
 fi
@@ -46,9 +38,37 @@ echo -e "容器启动成功..."
 echo -e "############################################################\n"
 
 
-  echo -e "##########8. 写入登陆信息 ############"
-  echo "{ \"username\": \"$ADMIN_USERNAME\", \"password\": \"$ADMIN_PASSWORD\" }" > /ql/data/config/auth.json
+echo -e "##########写入登陆信息############"
+#echo "{ \"username\": \"$ADMIN_USERNAME\", \"password\": \"$ADMIN_PASSWORD\" }" > /ql/data/config/auth.json
+dir_root=/ql && source /ql/shell/api.sh 
+init_auth_info() {
+  local body="$1"
+  local tip="$2"
+  local currentTimeStamp=$(date +%s)
+  local api=$(
+    curl -s --noproxy "*" "http://0.0.0.0:5600/api/user/init?t=$currentTimeStamp" \
+      -X 'PUT' \
+      -H "Accept: application/json" \
+      -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36" \
+      -H "Content-Type: application/json;charset=UTF-8" \
+      -H "Origin: http://0.0.0.0:5700" \
+      -H "Referer: http://0.0.0.0:5700/crontab" \
+      -H "Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7" \
+      --data-raw "{$body}" \
+      --compressed
+  )
+  code=$(echo "$api" | jq -r .code)
+  message=$(echo "$api" | jq -r .message)
+  if [[ $code == 200 ]]; then
+    echo -e "${tip}成功🎉"
+  else
+    echo -e "${tip}失败(${message})"
+  fi
+}
 
+init_auth_info "\"username\": \"$ADMIN_USERNAME\", \"password\": \"$ADMIN_PASSWORD\"" "Change Password"
+
+if [ -n "$RCLONE_CONF" ]; then
   echo -e "##########同步备份############"
   # 指定远程文件夹路径，格式为 remote:path
   REMOTE_FOLDER="huggingface:/qinglong"
