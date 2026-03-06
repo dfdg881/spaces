bf7dfdee681b5a86ca6b8fed348e1c81821ab771
dfdg881 <114667012+dfdg881@users.noreply.github.com>
2026-03-07 02:31:34 +0800

    更新 restore.sh

--- Baihu/restore.sh
+++ Baihu/restore.sh
@@ -58,7 +58,7 @@ if [ -n "$RCLONE_CONF" ]; then
       latest_file=$(rclone lsjson $REMOTE_FOLDER | jq -r 'sort_by(.ModTime) | last | .Path')
       # 复制到目标目录
       rclone copy $REMOTE_FOLDER/$latest_file /app/backup_tmp
-      RESTORE_RESPON=$(baihu restore $latest_file)
+      RESTORE_RESPON=$(baihu restore /app/backup_tmp/$latest_file)
       sleep 20
       rm -rf /app/backup_tmp
       sleep 20
